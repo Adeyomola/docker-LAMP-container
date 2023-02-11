2f72811387de490ee63b6d316eb43d9a060a91be
Adeyomola <kayzeel@yahoo.com>
2023-02-11 10:23:13 +0100

    Removed one of the scripts

--- scripts/createdb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mysql -u root -p= -e 'CREATE DATABASE altschool'
-mysql -u root -p= -e "CREATE USER adeyomola IDENTIFIED BY '$(cat /secrets/mysql_password)'"
-mysql -u root -p= -e "GRANT ALL ON altschool.* TO adeyomola@localhost IDENTIFIED BY '$(cat /secrets/mysql_password)'"
-mysql -u root -p= -e "FLUSH PRIVILEGES"
-mysql -u root -p= -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$(cat /secrets/mysql_root)'"
