46a0ecd8be0aaffae7035ba365901b5bd9d85d2a
Adeyomola <kayzeel@yahoo.com>
2023-02-19 22:44:44 +0100

    Switched to .env

--- scripts/env_editor.sh
+++ scripts/env_editor.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-sed -i 's|APP_URL.\+|APP_URL=http://localhost|' .env
-sed -i 's/APP_PORT.\+/#APP_PORT/' .env
-sed -i 's/DB_CONNECTION.\+/DB_CONNECTION=mysql/' .env
-sed -i 's/DB_HOST.\+/DB_HOST=mysql/' .env
-sed -i 's/DB_PORT.\+/DB_PORT=3306/' .env
-sed -i 's|DB_DATABASE.\+|echo "DB_DATABASE="$(cat /.secrets/mysql_db)|e' .env
-sed -i 's|DB_USERNAME.\+|echo "DB_USERNAME="$(cat /.secrets/mysql_user)|e' .env
-sed -i 's|DB_PASSWORD.\+|echo "DB_PASSWORD="$(cat /.secrets/mysql_password)|e' .env
+sed -i "s|APP_URL.\+|APP_URL=http://localhost|" .env
+sed -i "s/APP_PORT.\+/#APP_PORT/" .env
+sed -i "s/DB_CONNECTION.\+/DB_CONNECTION=mysql/" .env
+sed -i "s|DB_HOST.\+|DB_HOST=$DB_HOST|" .env
+sed -i "s|DB_PORT.\+|DB_PORT=$DB_PORT|" .env
+sed -i "s|DB_DATABASE.\+|DB_DATABASE=$DB_DATABASE|" .env
+sed -i "s|DB_USERNAME.\+|DB_USERNAME=$DB_USERNAME|" .env
+sed -i "s|DB_PASSWORD.\+|DB_PASSWORD=$DB_PASSWORD|" .env
